1448c488376dd81bf9a8f95493eb3e840c2ec781
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-01 17:45:14 +0500

    Added search functionality

--- script.js
+++ script.js
@@ -10,103 +10,95 @@ const retry = document.querySelector("#retryBtn");
 
 let allProducts = [];
 
-async function fetchProducts()
-{
-    try
-    {
-        showLoading();
-        hideError();
-        const response = await fetch("https://fakestoreapi.com/products");
+async function fetchProducts() {
+  try {
+    showLoading();
+    hideError();
+    const response = await fetch("https://fakestoreapi.com/products");
 
-        if(!response.ok)
-        {
-            throw new Error("Page Not Found");
-        }
-
-        const data = await response.json();
-
-        allProducts = data;
-        hideLoading();
-        display(allProducts);
+    if (!response.ok) {
+      throw new Error("Page Not Found");
     }
-    catch(err)
-    {
-        hideLoading();
-        console.log(err);
-        showError();
-    }
+
+    const data = await response.json();
+
+    allProducts = data;
+    hideLoading();
+    display(allProducts);
+  } catch (err) {
+    hideLoading();
+    console.log(err);
+    showError();
+  }
 }
 
-function showLoading()
-{
-    loading.classList.remove("hidden");
+function showLoading() {
+  loading.classList.remove("hidden");
 }
 
-function hideLoading()
-{
-    loading.classList.add("hidden");
+function hideLoading() {
+  loading.classList.add("hidden");
 }
 
-function showError()
-{
-    error.classList.remove("hidden");
+function showError() {
+  error.classList.remove("hidden");
 }
 
-function hideError()
-{
-    error.classList.add("hidden");
+function hideError() {
+  error.classList.add("hidden");
 }
 
-function display(products)
-{
-    productContainer.innerHTML = "";
+function display(products) {
+  productContainer.innerHTML = "";
 
-    products.forEach((product) => {
-        const card = document.createElement("div");
-        card.classList.add("card");
+  products.forEach((product) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
 
-        const image = document.createElement("img");
-        image.src = product.image;
-        image.alt = product.title;
+    const image = document.createElement("img");
+    image.src = product.image;
+    image.alt = product.title;
 
-        const title = document.createElement("h3");
-        title.textContent = product.title;
+    const title = document.createElement("h3");
+    title.textContent = product.title;
 
-        const categoryText  = document.createElement("p");
-        categoryText.textContent = product.category;
+    const categoryText = document.createElement("p");
+    categoryText.textContent = product.category;
 
-        const rating = document.createElement("p");
-        rating.textContent = `⭐ ${product.rating.rate} (${product.rating.count})`;
+    const rating = document.createElement("p");
+    rating.textContent = `⭐ ${product.rating.rate} (${product.rating.count})`;
 
-        
-        const price = document.createElement("h2");
-        price.textContent = `$${product.price}`;
+    const price = document.createElement("h2");
+    price.textContent = `$${product.price}`;
 
-        const button = document.createElement("button");
-        button.textContent = "View Details";
-         
-         card.append(
-            image,
-            title,
-            categoryText,
-            rating,
-            price,
-            button
-        );
+    const button = document.createElement("button");
+    button.textContent = "View Details";
 
-        productContainer.appendChild(card);
-    });
+    card.append(image, title, categoryText, rating, price, button);
 
-    counter.textContent = `Showing ${products.length} products`;
+    productContainer.appendChild(card);
+  });
 
-    if (products.length === 0)
-    {
-        empty.classList.remove("hidden");
-    }
-    else
-    {
-        empty.classList.add("hidden");
-    }
+  counter.textContent = `Showing ${products.length} products`;
+
+  if (products.length === 0) {
+    empty.classList.remove("hidden");
+  } else {
+    empty.classList.add("hidden");
+  }
 }
 
 fetchProducts();
+
+search.addEventListener("input", () => {
+  const searchText = search.value;
+  let lowerSearch = searchText.toLowerCase();
+
+  const filtered = allProducts.filter((product) => {
+    let lowerProduct = product.title.toLowerCase();
+
+    return lowerProduct.includes(lowerSearch) ;
+  });
+
+  display(filtered);
+});
