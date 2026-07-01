16fcf8c50f786eea376c91cf02ebea57264223fa
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-07-01 12:01:52 +0500

    JS: Added Variables

---
+++ script.js
@@ -0,0 +1,11 @@
+const search = document.querySelector("#search");
+const category = document.querySelector("#category");
+const sort = document.querySelector("#sort");
+const products = document.querySelector(".products");
+const counter = document.querySelector(".counter");
+const loading = document.querySelector(".loading");
+const error = document.querySelector(".error");
+const empty = document.querySelector(".empty");
+const retry = document.querySelector("#retryBtn");
+
+let allProducts = [];
